e013d7e55ed5ba168db6e92c277392ff3aa7ff04
SPIRITLOCAL\geminih <gemini612gemini@gmail.com>
2018-07-27 11:22:07 -0500

    1. Added Timer to refresh list every five min. 2. Added ProgressDialog before displaying list;

--- app/src/main/java/com/spiritfitness/spiritfitapp/PickUpZoneActivity.java
+++ app/src/main/java/com/spiritfitness/spiritfitapp/PickUpZoneActivity.java
@@ -1,21 +1,23 @@
 package com.spiritfitness.spiritfitapp;
 
+import android.app.ProgressDialog;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.widget.ListView;
 
-import com.spiritfitness.spiritfitapp.adapter.NormalRecyclerViewAdapter;
-import com.spiritfitness.spiritfitapp.adapter.PickUpZoneAdapter;
-import com.spiritfitness.spiritfitapp.common.Constants;
+import com.spiritfitness.spiritfitapp.adapter.PickupZoneRecyclerViewAdapter;
 import com.spiritfitness.spiritfitapp.model.PickUpZoneMap;
 import com.spiritfitness.spiritfitapp.restful.callback.ModelZoneMapCallback;
 import com.spiritfitness.spiritfitapp.restfulapi.APIClient;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,12 +25,13 @@ import retrofit2.Response;
 
 public class PickUpZoneActivity extends AppCompatActivity {
 
+    private Timer mTimer;
     private ModelZoneMapCallback modelZoneMapCallback;
    // private ListView listView;
-    private PickUpZoneAdapter pickUpZoneAdapter;
-    private NormalRecyclerViewAdapter normalRecyclerViewAdapter;
+    private PickupZoneRecyclerViewAdapter pickupZoneRecyclerViewAdapter;
     private List<PickUpZoneMap> pickupData;
 
+    private ProgressDialog progressDialog_loading;
     private RecyclerView mRecyclerView;
 
     @Override
@@ -46,13 +49,46 @@ public class PickUpZoneActivity extends AppCompatActivity {
         modelZoneMapCallback= APIClient.getClient().create(ModelZoneMapCallback.class);
 
 
-        getPickupZone();
+        mTimer = new Timer();
+        // start timer task
+        setTimerTask();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
+    }
+
+    private void setTimerTask() {
+        mTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Message message = new Message();
+                message.what = 1;
+                Log.e("Pickup","Updated !!");
+                doActionHandler.sendMessage(message);
+            }
+        }, 1000, 600*1000/* 表示1000毫秒之後，每隔1000毫秒執行一次 */);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(progressDialog_loading!=null) {
+            progressDialog_loading.dismiss();
+            progressDialog_loading =null;
+        }
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if(progressDialog_loading!=null) {
+            progressDialog_loading.dismiss();
+            progressDialog_loading =null;
+        }
     }
 
     private void getPickupZone()
@@ -76,9 +112,13 @@ public class PickUpZoneActivity extends AppCompatActivity {
                     pickupData.add(bean);
                 }
                 //listView.setAdapter(pickUpZoneAdapter);
-                normalRecyclerViewAdapter = new NormalRecyclerViewAdapter(PickUpZoneActivity.this, pickupData);
+                if(progressDialog_loading!=null) {
+                    progressDialog_loading.dismiss();
+                    progressDialog_loading=null;
+                }
+                pickupZoneRecyclerViewAdapter = new PickupZoneRecyclerViewAdapter(PickUpZoneActivity.this, pickupData);
                 mRecyclerView.setLayoutManager(new LinearLayoutManager(PickUpZoneActivity.this));
-                mRecyclerView.setAdapter(normalRecyclerViewAdapter);
+                mRecyclerView.setAdapter(pickupZoneRecyclerViewAdapter);
                // normalRecyclerViewAdapter.notifyDataSetChanged();
             }
 
@@ -88,6 +128,26 @@ public class PickUpZoneActivity extends AppCompatActivity {
             }
         });
     }
+
+    private Handler doActionHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            int msgId = msg.what;
+            switch (msgId) {
+                case 1:
+                    if (progressDialog_loading==null) {
+                        progressDialog_loading = ProgressDialog.show(PickUpZoneActivity.this, "",
+                                "Loading. Please wait...", true);
+                    }
+                    getPickupZone();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
 
 
 
