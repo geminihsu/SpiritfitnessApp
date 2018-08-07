583265c7bf6d02167ff141a852d1fe2cf0510617
SPIRITLOCAL\geminih <gemini612gemini@gmail.com>
2018-08-07 17:45:55 -0500

    1. Added zoneItemInfo model. 2. Uploaded screen shot.

---
+++ app/src/main/java/com/spiritfitness/spiritfitapp/restfulapi/ZoneItemInfo.java
@@ -0,0 +1,37 @@
+package com.spiritfitness.spiritfitapp.restfulapi;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import com.spiritfitness.spiritfitapp.model.Item_inventory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by User on 5/1/2017.
+ */
+
+public class ZoneItemInfo {
+
+
+
+    @SerializedName("seq")
+    public Integer seq;
+    @SerializedName("SN")
+    public String SN;
+    @SerializedName("date")
+    public String date;
+    @SerializedName("Location")
+    public String Location;
+    @SerializedName("ModelNo")
+    public String ModelNo;
+
+
+    @Override
+    public String toString() {
+        return "Feed{" +
+                "Item SN ='" + SN + '\'' +
+                ", Item Location=" + Location +
+                '}';
+    }
+}
